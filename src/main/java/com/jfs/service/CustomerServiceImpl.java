2961f2995e113db611389c9069aeb47c34cf523f
varunShekhar <varunshekhar71@gmail.com>
2019-08-17 20:33:39 +0530

    changes in CustomerServiceImpl.java where @Autowired is removed above variable or object to above the setter that we generated using setter and getter option.

--- src/main/java/com/jfs/service/CustomerServiceImpl.java
+++ src/main/java/com/jfs/service/CustomerServiceImpl.java
@@ -28,8 +28,18 @@ import com.jfs.repository.CustomerRepositoryImpl;
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 	//private CustomerRepository customerRepository = new CustomerRepositoryImpl();
-	@Autowired
+	//@Autowired
 	private CustomerRepository customerRepository;
+	
+	public CustomerRepository getCustomerRepository() {
+		return customerRepository;
+	}
+	@Autowired
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		System.out.println("We are using setter injection");
+		this.customerRepository = customerRepository;
+	}
+
 	@Override
 	public ArrayList<Customer> findAll() {
 		return customerRepository.findAll();
